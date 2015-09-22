dfeafa52b6eedadcd4e1a44e1af0b537518790d4
usuario <usuario@10.22.143.9>
2015-09-22 10:12:23 +0100

    Tercera Versión

--- src/kata/KATA.java
+++ src/kata/KATA.java
@@ -4,7 +4,9 @@
  */
 package kata;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  *
@@ -16,16 +18,20 @@ public class KATA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-
+        System.out.println("Hola");
+        System.out.println("...");
         Person p1;
-        Date f; 
-                
-        f = new Date(64, 7, 3);
+        //Date f; 
+        //f = new Date(64, 7, 3);
+        
+        Calendar f;
+        f = GregorianCalendar.getInstance();
+        f.set(1964,7,3);
+        
         p1 = new Person("Enrique", "Ramón", f);
         
         System.out.println(p1.getFullName());
-        System.out.println(p1.getBirthday());
+        System.out.println(p1.getBirthday().getTime());
         System.out.println(p1.getAge());
         System.out.println("...");
         System.out.println("Fin");
